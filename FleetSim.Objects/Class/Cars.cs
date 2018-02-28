643fc5665f34d98b829d3b98a039b10a2266361e
ahmedali1994 <33011828+ahmedali1994@users.noreply.github.com>
2018-02-27 23:08:52 -0500

    Add files via upload

---
+++ FleetSim.Objects/Class/Cars.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FleetSim.Objects;
+using FleetSim.Objects.Interface;
+
+namespace FleetSim.Objects.Class
+{
+    public class Cars : Vehicle, ICars
+    {
+        int year;
+        string Model;
+
+        public Cars (String ModelName)
+        {
+            Model = ModelName;
+
+            
+        }
+        public Cars (string ModelName, string CarColor, int Year)
+        {
+            year = Year;
+            Model = ModelName;
+            VehicleColor = CarColor;
+
+        }
+        public void TuneUp() {
+            int Result = (Mileages - LastServicedMileages);
+            if (Result >= 30000)
+            {
+                LastServicedMileages = Mileages;
+                LastServicedDate = DateTime.Now;
+                Console.WriteLine("Vehicle tuned up at " + LastServicedMileages + "miles on " + LastServicedDate + " " + Model);
+                if (Result >= 100000)
+                {
+                    RebulidEngine();
+                }
+
+            }
+
+        }
+        public void RebulidEngine()
+        {
+            Mileages = 0;
+            LastServicedMileages = 0;
+            Console.WriteLine("Engine rebuild on "+ LastServicedDate + "for " + Model);
+
+
+        }
+
+    }
+
+    
+}
---
+++ FleetSim.Objects/Class/Vehicle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetSim.Objects.Class
+{
+    
+    
+       // enum Color:byte {Red, White, Blue }
+    
+     public class Vehicle
+     {
+        protected Guid VIN;
+        protected int Mileages;
+        protected string VehicleColor;
+        protected DateTime LastServicedDate;
+        protected int LastServicedMileages;
+
+        public int MileagNbr
+        {
+            get { return Mileages;  }
+            set { Mileages = value; }
+        }
+        public Guid VinNbr
+        {
+            get { return VIN; }
+            set { VIN = value; }
+        }
+
+    }
+    
+
+}
